8c17c615be5b4d8ae131133e9e08dbb6216dd1f9
Tam-Kun <bnbgg37@gmail.com>
2022-08-14 19:53:11 +0900

    serve and copy

--- docs/sw.js
+++ docs/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220814.193849';
+const cacheName = 'chirpy-20220814.195131';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
