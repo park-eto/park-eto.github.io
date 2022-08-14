c61c56de495bdb076df607b540f18ddfa9dac137
Tam-Kun <bnbgg37@gmail.com>
2022-08-14 20:07:07 +0900

    aaa

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220814.195131';
+const cacheName = 'chirpy-20220814.200626';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
