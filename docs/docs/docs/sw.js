cc98cf9747e4980304dec1b7ffc5fe43a4d8a023
Tam-Kun <bnbgg37@gmail.com>
2022-08-14 19:50:18 +0900

    docs update

--- docs/docs/docs/sw.js
+++ docs/docs/docs/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220814.193247';
+const cacheName = 'chirpy-20220814.191633';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
